b778e26da6caa722e5cf43f23f099bb00622d115
Irving Barajas <irving@thecoursekey.com>
2018-12-25 21:20:16 -0800

    [Feat] Higher or Lower functionality
    - deck creation
    - shuffle
    - higher or lower

--- screens/GameView.js
+++ screens/GameView.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import { View, StyleSheet } from 'react-native';
 import Header from '../components/Header';
 import Card from '../components/Card';
 import shuffle from '../helpers/shuffle';
@@ -15,34 +15,12 @@ export default class GameView extends React.Component {
     nextCard: null,
     deckOfCards: [],
   };
-  handleHigherLowerSelect = (option) => {
-    const { isSpotLight, multiplier, currentCard, nextCard } = this.state;
-    console.log('current/next', currentCard.value, nextCard.value);
-    console.log('option', this.isHigherOrLower(option));
-    if (this.isHigherOrLower(option)) {
-      alert('Correct!');
-    } else {
-      alert('Wrong!');
-    }
-    // go to next card
-    // go to next player
-  };
-  isHigherOrLower = (option) => {
-    const { currentCard, nextCard } = this.state;
-    if (option === 'higher') {
-      if (currentCard.value < nextCard.value) return true;
-    } else { // option === 'lower'
-      if (currentCard.value > nextCard.value) return true;
-    }
-    return false;
-  };
-
   componentDidMount() {
     const deckOfCards = deck();
 
     this.setState(() => {
       const currentCard = shuffle(deckOfCards).pop();
-      const nextCard = deckOfCards.pop();
+      const nextCard = deckOfCards[0];
       const cardsAlreadyUsed = [currentCard];
 
       return {
@@ -53,6 +31,69 @@ export default class GameView extends React.Component {
       };
     });
   }
+  handleHigherLowerSelect = (option) => {
+    const { currentCard, nextCard } = this.state;
+    if (this.isHigherOrLower(option)) {
+      console.log(`Correct: ${nextCard.value} is ${option} then ${currentCard.value}`);
+      // alert(`Correct: ${nextCard.value} is ${option} then ${currentCard.value}`);
+    } else {
+      console.log(`Incorrect: ${nextCard.value} is NOT ${option} then ${currentCard.value}`);
+      // alert(`Incorrect: ${nextCard.value} is NOT ${option} then ${currentCard.value}`);
+    }
+    this.goToNextCard();
+    this.goToNextPlayer()
+  };
+  goToNextPlayer = () => {
+    const { currentPlayerIndex } = this.state;
+    const { players } = this.props.navigation.state.params;
+
+    if (currentPlayerIndex + 1 < players.length) {
+      this.setState((prevState) => {
+        return { currentPlayerIndex: prevState.currentPlayerIndex + 1 };
+      })
+    } else {
+      this.setState(() => {
+        return { currentPlayerIndex: 0 };
+      })
+    }
+  };
+  goToNextCard = () => {
+    const { deckOfCards, cardsAlreadyUsed } = this.state;
+
+    this.setState(() => {
+      const newCurrentCard = deckOfCards[0];
+      const newNextCard = deckOfCards[1];
+
+      // remove first card from deck and add it to the cardsAlreadyUsed pile
+      let newDeckOfCards = deckOfCards.filter((item, i) => i !== 0);
+      let newCardsAlreadyUsed = cardsAlreadyUsed.concat(newCurrentCard);
+
+      // re-shuffle cardsAlreadyUsed back into deck
+      if (newDeckOfCards.length === 1) {
+        const tempNewDeck = shuffle(cardsAlreadyUsed);
+        // add the last card back into the deck
+        tempNewDeck.concat(newDeckOfCards[0]);
+        newDeckOfCards = tempNewDeck;
+        // add current and next cards into cards already in use
+        newCardsAlreadyUsed = [newCurrentCard, newNextCard];
+      }
+      return {
+        currentCard: newCurrentCard,
+        nextCard: newNextCard,
+        deckOfCards: newDeckOfCards,
+        cardsAlreadyUsed: newCardsAlreadyUsed,
+      };
+    });
+  };
+  isHigherOrLower = (option) => {
+    const { currentCard, nextCard } = this.state;
+    if (option === 'higher') {
+      if (currentCard.value < nextCard.value) return true;
+    } else { // option === 'lower'
+      if (currentCard.value > nextCard.value) return true;
+    }
+    return false;
+  };
 
   render() {
     const { isSpotLight, multiplier, currentPlayerIndex, currentCard } = this.state;
